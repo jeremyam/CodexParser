b3219fa62a34a2d7ec2b1b14745a349879a46c30
Jeremy Menicucci <menicucci_207@yahoo.com>
2024-03-13 21:25:12 -0400

    Tweaked regex'

--- src/regex.js
+++ src/regex.js
@@ -3,5 +3,5 @@ export const books =
 export const abbrBooks =
     /(?:(Gen|Exo|Lev|Num|Deu|Jos|Jdg|Rut|1 Sa|2 Sa|1 Kgs|2 Kgs|1 Chr|2 Chr|Ezr|Neh|Est|Job|Psa|Pro|Ecc|Son|Isa|Jer|Lam|Eze|Dan|Hos|Joe|Amo|Oba|Jon|Mic|Nah|Hab|Zep|Hag|Zec|Mal|Matt|Mar|Luk|Joh|Act|Rom|1 Cor|2 Cor|Gal|Eph|Phi|Col|1 Thess|2 Thess|1 Ti|2 Ti|Tit|Phm|Heb|Jam|1 Pe|2 Pe|1 Jo|2 Jo|3 Jo|Jud|Rev))/g
 export const chapter = /(?:\s?\d+:?)/g
-export const verse = /(?:\s?\d+(?:-\d+)?)/g
+export const verse = /(?:\s?\d+(?:-?—?–?,?;?\s?\d+)?)/g
 export const scripturesRegex = new RegExp(`(${books.source})(${chapter.source})?(${verse.source})`, "gm")
--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 import CodexParser from './src/CodexParser.js'
 
 const parser = new CodexParser();
-const text = 'The passages that we are looking at tonight are found in 1 John 3:16, and Romans 10:13'
-console.log(parser.parse(text))
+const text = 'The passages that we are looking at tonight are found in 1 John 3:16, and Romans 10:13, 15'
+const single = 'John 3:16'
+parser.parse(single)
+console.log(parser)
