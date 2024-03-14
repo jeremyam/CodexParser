9fdf9fb0e0a6ad9f31e60320ddaaefe7629a11b8
Jeremy Menicucci <menicucci_207@yahoo.com>
2024-03-13 21:55:58 -0400

    Fine tuned the regex and parser

--- src/CodexParser.js
+++ src/CodexParser.js
@@ -5,13 +5,17 @@ export default class CodexParser {
      * Constructor for creating an instance of the class.
      */
     constructor() {
+        this.found = []
         this.passages = []
         this.bible = bible
         this.nt = false
         this.ot = false
         this.bookRegex = regex.books
         this.bookAbbrRegex = regex.abbrBooks
+        this.chapterRegex = regex.chapter
+        this.verseRegex = regex.verse
         this.scripturesRegex = regex.scripturesRegex
+        this.abbrScripturesRegex = regex.abbrScripturesRegex
     }
     /**
      * Parses the passage using the bookRegex.
@@ -19,11 +23,39 @@ export default class CodexParser {
      * @param {string} passage - the passage to be parsed
      * @return {Array|null} an array of matches or null if there are no matches
      */
-    parse(passage) {    
+    parse(passage) {
         this.scan(passage)
+        for (let i = 0; i < this.found.length; i++) {
+            const passage = {
+                original: this.found[i],
+                book: this.found[i].match(this.bookRegex)[0],
+                chapter: this.found[i].replace(this.bookRegex).match(this.chapterRegex)[0].replace(":", "").trim(),
+                verse: this.found[i].match(this.verseRegex)[0].replace(":", "").trim(),
+            }
+            if(/,/.test(passage.verse)){
+                passage.verse = passage.verse.split(/,/)
+            }
+            this.passages.push(passage)
+        }
     }
-    
+
+    /**
+     * Scans the input text and stores the matching passages in the 'passages' property.
+     *
+     * @param {string} text - The input text to scan.
+     * @return {Array}
+     */
     scan(text) {
-        this.passages = text.match(this.scripturesRegex)
+        this.found = text.match(this.scripturesRegex)
+        return this.found
+    }
+
+    /**
+     * Get the passages.
+     *
+     * @return {Array} The passages.
+     */
+    getPassages() {
+        return this.passages
     }
 }
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 import CodexParser from './src/CodexParser.js'
 
 const parser = new CodexParser();
-const text = 'The passages that we are looking at tonight are found in 1 John 3:16, and Romans 10:13, 15'
+const text = 'The passages that we are looking at tonight are found in 1 John 3:16, and Romans 10:13-15. Please turn in your Bibles.'
 const single = 'John 3:16'
-parser.parse(single)
-console.log(parser)
+parser.parse(text)
+console.log(parser.getPassages())
