f5b92aefba1bd9a3f0db43ef63c3d7601631de8b
Jeremy Menicucci <menicucci_207@yahoo.com>
2024-05-25 14:57:16 -0400

    Now finds abbr. multiples with no spaces between number and name

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const CodexParser = require("./src/CodexParser.js")
 const parser = new CodexParser()
 const text =
     "The passages that we are looking at tonight are found in 1 John 3:16-17, 1 Peter 1:1, and Romans 10:13, 15, 17. Please turn in your Bibles."
-const single = "Mt 2.9"
+const single = "1Cor 2.9"
 parser.parse(single)
 //parser.parse(single)
 console.log(parser.getPassages())
