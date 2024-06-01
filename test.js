9861116b6083a837b50e9f780b151c1714e844ce
Jeremy Menicucci <menicucci_207@yahoo.com>
2024-06-01 16:48:36 -0400

    Editing the books.

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const CodexParser = require("./src/CodexParser.js")
 
 const parser = new CodexParser()
 const text =
-    "The passages that we are looking at tonight 1 Cor 12:34 2 Cor 3:4 are found Jude 6, in Jude 5, Genesis 2:1 - 3:19, 1 John 3:16-17, 1 Peter 1:1, and Romans 10:13, 15, 17. Please turn in your Bibles."
+    "The passages Luke 2:32 and Lk 1:23 that we are looking at tonight 1 Cor 12:34 2 Cor 3:4 are found Jude 6, in Jude 5, Genesis 2:1 - 3:19, 1 John 3:16-17, 1 Peter 1:1, and Romans 10:13, 15, 17. Please turn in your Bibles."
 const single = "Ge 27.27-29,89-40 Heb 11.20 Heb. 12.17"
 const jd = "Jd. 5"
 const cor = "1 Cor 12:34 2 Cor 3:4"
