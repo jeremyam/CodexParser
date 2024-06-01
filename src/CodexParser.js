2bb7d42ddcf5ac123dd238876423853c05987a17
Jeremy Menicucci <menicucci_207@yahoo.com>
2024-06-01 12:24:58 -0400

    Fixed parsings for jude

--- src/CodexParser.js
+++ src/CodexParser.js
@@ -23,7 +23,15 @@ class CodexParser {
      * @return {array} The found passages from the text.
      */
     scan(text) {
+        const judeRegex = /\b(?:[J|j][ude|d]+)\.?\s?\d+\b/gim
+        const jude = text.match(judeRegex)
         this.found = text.match(this.scripturesRegex)
+        if (!this.found) {
+            this.found = []
+        }
+        if (jude) {
+            this.found.push(...jude.map((item) => item.trim()))
+        }
         return this.found
     }
 
@@ -40,13 +48,20 @@ class CodexParser {
         this.passages = []
         this.scan(reference)
         for (let i = 0; i < this.found.length; i++) {
+            console.log(this.found[i])
             const hasChapterRange = this.found[i].match(/(?<=-\s?)\b\d+[.:].+\b/)
-            const book = this.found[i].match(this.bookRegex)
-            const chapter = this.found[i].replace(book[0], "").match(this.chapterRegex)
-            const verse = this.found[i].match(this.verseRegex)[0].replace(/[:.]/, "").trim()
+            const book = this.bookify(this.found[i].match(this.bookRegex))
+            let chapter, verse
+            if (book.toLowerCase() !== "jude") {
+                chapter = this.found[i].replace(book[0], "").match(this.chapterRegex)
+                verse = this.found[i].match(this.verseRegex)[0].replace(/[:.]/, "").trim()
+            } else {
+                chapter = 1
+                verse = this.found[i].replace(book[0], "").match(this.chapterRegex)
+            }
             const passage = {
                 original: this.found[i],
-                book: this.bookify(book),
+                book: book,
                 chapter: this.chapterify(chapter),
                 verses: verse,
             }
@@ -60,7 +75,12 @@ class CodexParser {
                 passage.to.verses = passage.to.verses.split(/,/).filter(Boolean)
                 passage.to.testament = this.bible.old.includes(passage.to.book) ? "old" : "new"
             }
-            passage.verses = passage.verses.split(/,/).filter(Boolean)
+            console.log(typeof passage.verses)
+            if (typeof passage.verses === "object" || typeof passage.verses === "array") {
+                passage.verses = passage.verses.map((verse) => verse.trim())
+            } else {
+                passage.verses.split(/,/).filter(Boolean)
+            }
             passage.testament = this.bible.old.includes(passage.book) ? "old" : "new"
             passage.scripture = this.scripturize(passage)
             this.passages.push(passage)
@@ -70,7 +90,8 @@ class CodexParser {
         return this.passages
     }
     chapterify(chapter) {
-        return chapter[0].replace(/[:\.]/, "").trim()
+        if (typeof chapter === "array") return chapter[0].replace(/[:\.]/, "").trim()
+        else return chapter
     }
 
     /**
--- test.js
+++ test.js
@@ -2,8 +2,9 @@ const CodexParser = require("./src/CodexParser.js")
 
 const parser = new CodexParser()
 const text =
-    "The passages that we are looking at tonight are found in Genesis 2:1 - 3:19, 1 John 3:16-17, 1 Peter 1:1, and Romans 10:13, 15, 17. Please turn in your Bibles."
+    "The passages that we are looking at tonight are found Jude 6, in Jude 5, Genesis 2:1 - 3:19, 1 John 3:16-17, 1 Peter 1:1, and Romans 10:13, 15, 17. Please turn in your Bibles."
 const single = "Ge 27.27-29,89-40 Heb 11.20"
+const jd = "Jd. 5"
 const passages = parser.parse(text)
 console.log(passages)
 
