c2c2a6d718d24cc4a7e2bba5f4e99f906a73114c
Jeremy Menicucci <menicucci_207@yahoo.com>
2024-06-03 12:12:39 -0400

    Resolved multi verses with no spacess

--- sub-verse-scripture-test.js
+++ sub-verse-scripture-test.js
@@ -2,4 +2,5 @@ const CodexParser = require("./src/CodexParser.js")
 
 const parser = new CodexParser()
 const text = parser.parse("Nehemiah 1:1d")
-console.log(text)
+const multiNoSpace = parser.parse('Ps 109:4,5,7,8')
+console.log(multiNoSpace)
