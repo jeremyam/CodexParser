44948bf8c2a1ba1bd0c8f6dcce720eb7b943e31e
jeremyam <jeremy@jeremyam.com>
2024-06-12 14:14:46 -0600

    Fixed issue with Song of Solomon

--- all-scripture-test.js
+++ all-scripture-test.js
@@ -5,14 +5,16 @@ const text =
     "Joel 10:13 The passages Luke 2:32 and Lk 1:23 that we are looking at tonight 1 Cor 12:34 2 Cor 3:4 are found Jude 6, in Jude 5, Genesis 2:1 - 3:19, 1 John 3:16-17, 1 Peter 1:1, and Romans 10:13, 15, 17. Please turn in your Bibles. Ps 109:4,5,6,8.  Isaiah 61.2-3 Mt 5.4"
 const single = "Ge 27.27-29,89-40 Heb 11.20 Heb. 12.17 Jonah 3"
 const jd = "Jd. 5"
-const cor = "1 Cor 12:34 2 Cor 3:4. He 4.12 Re 1.16 Leviticus 16.6 He 5.3 He 7.27"
+const cor = "Hos 1:1-3, 8 Song of Solomon 1:2, Song of Songs 2:2"
 const passages = parser.parse(single)
 console.log(passages)
 
 const jude = parser.parse(jd)
 console.log(jude)
-const textParser = parser.parse(cor)
-console.log(textParser)
+
 
 const fullText = parser.parse(text)
 console.log(fullText)
+
+const textParser = parser.parse(cor)
+console.log(textParser)
--- src/toc.js
+++ src/toc.js
@@ -1,5 +1,3 @@
 const fs = require("fs")
-
 const bible = JSON.parse(fs.readFileSync(__dirname + "/../bibles/updated_kjv.json", "utf8"))
-
 module.exports.bible = bible
