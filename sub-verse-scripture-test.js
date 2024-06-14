7aa4e4dd8f3f698ba58ba7a0e7bdaf05f97dc182
jeremyam <jeremy@jeremyam.com>
2024-06-14 15:46:06 -0600

    Updataed

--- sub-verse-scripture-test.js
+++ sub-verse-scripture-test.js
@@ -1,6 +1,6 @@
 const CodexParser = require("./src/CodexParser.js")
 
 const parser = new CodexParser()
-const scripture = "Hos 1:1-3, 8"
+const scripture = "He 1:1-3, 8"
 console.log(scripture)
 parser.parse(`${scripture}. Please turn in your Bibles.`)
