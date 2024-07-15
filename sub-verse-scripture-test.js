3d5c202cb06f15490e07fbab9dded6c41b6a3695
Jeremy Menicucci <menicucci_207@yahoo.com>
2024-07-15 18:58:46 -0400

    Updated

--- sub-verse-scripture-test.js
+++ sub-verse-scripture-test.js
@@ -1,5 +1,5 @@
 const BibleParser = require("./src/CodexParser.js")
-const string = "Revelation 12:3; 13:1"
+const string = "Revelation 4:9; 10:5; 12:14"
 const parser = new BibleParser()
 const result = parser.parse(string)
 console.log(result.getPassages())
