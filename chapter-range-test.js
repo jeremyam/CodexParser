ac95d9d89406f7511f2f2caf0efc73df0939280f
Jeremy Menicucci <menicucci_207@yahoo.com>
2024-07-15 18:59:13 -0400

    Updated

--- chapter-range-test.js
+++ chapter-range-test.js
@@ -2,4 +2,8 @@ const BibleParser = require("./src/CodexParser.js")
 const string = "Revelation 3:5; 7:14; 12:7; 13:8; 16:18; 20:1-6"
 const parser = new BibleParser()
 const passages = parser.parse(string)
+<<<<<<< HEAD
 //console.log(passages.getPassages())
+=======
+//console.log(passages.getPassages())
+>>>>>>> 24d85c2 (Updated)
