8bbfa9e42ba9da6f93b3e85617a7157656c52f0a
jeremyam <jeremy@jeremyam.com>
2024-08-12 11:18:14 -0400

    Updated

--- sub-verse-scripture-test.js
+++ sub-verse-scripture-test.js
@@ -1,5 +1,8 @@
 const BibleParser = require("./src/CodexParser.js")
-const string = "Rev 12:18"
+const string = "Psalms 128:6"
 const parser = new BibleParser()
+parser.options({
+    
+})
 const result = parser.parse(string)
 console.log(result.getPassages())
