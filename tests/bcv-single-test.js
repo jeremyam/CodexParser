33f53bc90a24bc00cd1a66c3c163dd5560120805
jeremyam <jeremy@jeremyam.com>
2024-09-04 14:34:04 -0400

    Updated

--- tests/bcv-single-test.js
+++ tests/bcv-single-test.js
@@ -4,16 +4,14 @@ const util = require("util")
 const dump = (item) => {
     console.log(util.inspect(item, { depth: null, colors: true }))
 }
-const text = "John 12:38 John 1:29,34 Isaiah 53:1 Isaiah 6:10 Isaiah 6:1 Isaiah 52:13 Isaiah 6:3 LXX Isaiah 52:13 LXX Isaiah 6:7 Isaiah 53:12 Isaiah 52:13 - 53:12"
+const text = "Genesis 22:1,2,4-6"
 
 const parser = new bcv_parser({
     invalid_sequence_strategy: "include",
     invalid_passage_strategy: "include",
-    sequence_combination_strategy: "combine"
+    sequence_combination_strategy: "combine",
 })
 const textParser = parser.parse(text)
 console.log(textParser)
 const passages = textParser.parsed_entities()
-passages.forEach((passage) => {
-    dump(passage)
-})
+dump(passages)
--- tests/single.js
+++ tests/single.js
@@ -2,9 +2,10 @@ const BibleParser = require("../src/CodexParser.js")
 const dump = require("../src/functions.js").dump
 
 const parser = new BibleParser()
+const text = "Genesis 22:1,2,4-6"
+const secondText = "Genesis 22:1-2"
+const textParsed = parser.parse(text).getPassages()
+const secondTextparsed = parser.parse(secondText).getPassages()
 
-const text = "Genesis 22:2,12 LXX"
-
-const passages = parser.parse(text).getPassages()
-
-dump(passages)
+dump(textParsed)
+dump(secondTextparsed)
