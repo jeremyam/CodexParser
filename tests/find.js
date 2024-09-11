c5ba64012af27f46adec9cddee2449dae9821cb5
jeremyam <jeremy@jeremyam.com>
2024-09-11 16:07:09 -0400

    splitting versifications into different files

--- tests/find.js
+++ tests/find.js
@@ -2,7 +2,7 @@ const CodexParser = require("../src/CodexParser.js")
 
 const parser = new CodexParser()
 
-parser.find('Psalm 2:2-13; 3:2-4 30:5 LXX').enhance()
+parser.find('Psalm 3:0').enhance()
 
 console.log(parser.getPassages())
 console.log(parser.getText())
--- tests/single.js
+++ tests/single.js
@@ -2,10 +2,7 @@ const BibleParser = require("../src/CodexParser.js")
 const dump = require("../src/functions.js").dump
 
 const parser = new BibleParser()
-const text = "Genesis 22:1,2,4-6"
-const secondText = "Genesis 22:1-2"
-const textParsed = parser.parse(text).getPassages()
+const secondText = "Psalm 3:0"
 const secondTextparsed = parser.parse(secondText).getPassages()
 
-dump(textParsed)
 dump(secondTextparsed)
