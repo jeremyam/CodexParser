0d5110838227c6d1b4a0a2f5662903262618bf81
Jeremy Menicucci <menicucci_207@yahoo.com>
2024-09-16 16:55:07 -0400

    Removed test dir

--- tests/single.js
+++
@@ -1,8 +0,0 @@
-const BibleParser = require("../src/CodexParser.js")
-const dump = require("../src/functions.js").dump
-
-const parser = new BibleParser()
-const secondText = "Psalm 89:2"
-const secondTextparsed = parser.parse(secondText).getPassages()
-console.log(secondText)
-dump(secondTextparsed)
--- tests/versification.js
+++
@@ -1,8 +0,0 @@
-const BibleParser = require("../src/CodexParser")
-const dump = require("../src/functions").dump
-
-const parser = new BibleParser()
-
-const passages = parser.parse("Exodus 8:1").getPassages()
-
-dump(passages)
