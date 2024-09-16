90b57b7deaeaf34a6db86bbf6f20ec28fad22c19
Jeremy Menicucci <menicucci_207@yahoo.com>
2024-09-16 16:53:32 -0400

    Updated

--- tests/single.js
+++ tests/single.js
@@ -2,7 +2,7 @@ const BibleParser = require("../src/CodexParser.js")
 const dump = require("../src/functions.js").dump
 
 const parser = new BibleParser()
-const secondText = "Psalm 89:2"
+const secondText = "Psalm 4:3"
 const secondTextparsed = parser.parse(secondText).getPassages()
 console.log(secondText)
 dump(secondTextparsed)
